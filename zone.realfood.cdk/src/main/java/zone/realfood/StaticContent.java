6eec313695a39ee4315ca32a5c4b104d513765fb
e-soul contrib1 <esoulorgcontrib1@gmail.com>
2025-09-06 10:02:42 +0200

    Add cors.

--- zone.realfood.cdk/src/main/java/zone/realfood/StaticContent.java
+++ zone.realfood.cdk/src/main/java/zone/realfood/StaticContent.java
@@ -7,6 +7,8 @@ import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.s3.BlockPublicAccess;
 import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.CorsRule;
+import software.amazon.awscdk.services.s3.HttpMethods;
 import software.amazon.awscdk.services.s3.deployment.BucketDeployment;
 import software.amazon.awscdk.services.s3.deployment.CacheControl;
 import software.amazon.awscdk.services.s3.deployment.Source;
@@ -21,18 +23,14 @@ public class StaticContent extends Stack {
         String region = Stack.of(this).getRegion();
         String bucketName = String.format("realfood-zone-static-%s-%s", account, region);
 
-        Bucket bucket = Bucket.Builder.create(this, "StaticAssetsBucket")
-                .bucketName(bucketName)
-                .blockPublicAccess(BlockPublicAccess.BLOCK_ACLS_ONLY)
-                .publicReadAccess(true)
-                .enforceSsl(true)
+        List<String> origins = List.of("https://realfood.zone", "https://www.realfood.zone", "https://beta.realfood.zone");
+        Bucket bucket = Bucket.Builder.create(this, "StaticAssetsBucket").bucketName(bucketName).blockPublicAccess(BlockPublicAccess.BLOCK_ACLS_ONLY)
+                .publicReadAccess(true).enforceSsl(true)
+                .cors(List.of(CorsRule.builder().allowedOrigins(origins).allowedMethods(List.of(HttpMethods.GET, HttpMethods.HEAD)).allowedHeaders(List.of("*"))
+                        .exposedHeaders(List.of("ETag")).maxAge(3000).build()))
                 .build();
 
-    BucketDeployment.Builder.create(this, "DeployStaticAssets")
-        // Deploy everything from the consolidated static-content folder
-        .sources(List.of(Source.asset("static-content")))
-                .destinationBucket(bucket)
-                .cacheControl(List.of(CacheControl.maxAge(Duration.days(30))))
-                .build();
+        BucketDeployment.Builder.create(this, "DeployStaticAssets").sources(List.of(Source.asset("static-content"))).destinationBucket(bucket)
+                .cacheControl(List.of(CacheControl.maxAge(Duration.days(30)))).build();
     }
 }
